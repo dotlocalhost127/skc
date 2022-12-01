cf39a320d595ecf38ed84d8398af1542db3f1440
dotlocalhost127 <dotlocalhost127@gmail.com>
2022-12-01 22:42:26 +0500

    Update AccountsController.php

--- app/Http/Controllers/Accounts/AccountsController.php
+++ app/Http/Controllers/Accounts/AccountsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Accounts;
 use App\Http\Controllers\Controller;
 use App\Models\accounts;
 use App\Models\c;
+use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 
 class AccountsController extends Controller
@@ -12,6 +13,12 @@ class AccountsController extends Controller
        
    public function ViewAccounts(){
     $viewaccount = accounts::all();
+    $balance = new accounts( Create::
+
+
+
+
+    );
     return view('backend.accounts.view_account', compact('viewaccount'));
 
    }
@@ -23,19 +30,15 @@ class AccountsController extends Controller
    }
 
    public function StoreAccounts(request $request){
-    $addaccount = accounts::all();
+    $addaccount = new accounts;
     $addaccount->reference= $request->reference;
     $addaccount->date=$request->date;
     $addaccount->particulars=$request->particulars;
     $addaccount->debit=$request->debit;
     $addaccount->credit=$request->credit;
-    $credit = $request->credit;
-    $debit = $request->debit;
-    $balance = $credit - $debit;
-    $addaccount->$balance=$request->balance;
    $addaccount->save();
     $notification = array(
-        'message' => 'Accounts  added  added successfully',
+        'message' => 'Accounts    added successfully',
         'alert-type' => 'success'
     );
     return redirect()->route('accounts.view_account')->with($notification);
